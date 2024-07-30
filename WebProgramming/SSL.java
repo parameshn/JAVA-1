ac0bd7a2afbfa31cee2bd807dcca94c91d66b020
paramesh <parameshdarshan1234@gmail.com>
2024-07-30 23:42:56 +0530

    SSL and Secure Web Communications

---
+++ WebProgramming/SSL.java
@@ -0,0 +1,25 @@
+package WebProgramming;
+
+public class SSL {
+
+    /*SSL and Secure Web Communications
+    The previous examples sent a field called Password to the server. However, standard
+    HTTP doesn’t provide encryption to hide our data. Fortunately, adding security for
+    GET and POST operations like this is easy (trivial, in fact, for the client-side developer).
+    Where available, you simply need to use a secure form of the HTTP protocol—
+    HTTPS:
+    https://www.myserver.example/cgi-bin/login.cgi
+    HTTPS is a version of the standard HTTP protocol run over Secure Sockets Layer
+    (SSL), which uses public-key encryption techniques to encrypt the browser-to-server
+    communications. Most web browsers and servers currently come with built-in sup‐
+    port for HTTPS (or raw SSL sockets). Therefore, if your web server supports HTTPS
+    and has it configured, you can use a browser to send and receive secure data simply
+    by specifying the https protocol in your URLs. There is much more to learn about
+    SSL and related aspects of security, such as authenticating whom you are actually talk‐
+    ing to, but as far as basic data encryption goes, this is all you have to do. It is not
+    something your code has to deal with directly. The Java JRE standard edition ships
+    with SSL and HTTPS support, and beginning with Java 5.0, all Java implementations
+    must support HTTPS as well as HTTP for URL connections
+    */
+    
+}
